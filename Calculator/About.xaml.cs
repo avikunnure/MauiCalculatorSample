ad23c7ba09076b877e9945d0d343960d276a20e3
avikunnure <avinashkunnure@gmail.com>
2023-02-24 18:54:05 +0530

    Added theme change options

--- Calculator/About.xaml.cs
+++ Calculator/About.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using Calculator.Resources;
+
+namespace Calculator
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class About : ContentPage
@@ -10,16 +12,40 @@
         }
         public void OnBackToCalculator(object sender,EventArgs args)
         {
-           Application.Current.MainPage.Navigation.PopModalAsync();
+           Application.Current.MainPage.Navigation.PopAsync();
         }
         public void ExitApp(object sender, EventArgs e)
         {
             Application.Current.Quit();
         }
-        public void OnBackToAbout(object sender, EventArgs args)
+       
+        public void OnSetThemes(object sender, EventArgs args)
         {
-            var name = Application.Current.StyleId;
-            Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new About()), true);
+
+            MenuFlyoutItem menu = (MenuFlyoutItem)sender;
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            if (mergedDictionaries != null)
+            {
+                switch (menu.CommandParameter)
+                {
+                    case "1":
+                        mergedDictionaries.Clear();
+                        mergedDictionaries.Add(new LightTheme());
+                        break;
+                    case "2":
+                        mergedDictionaries.Clear();
+                        mergedDictionaries.Add(new PinkTheme());
+                        break;
+                    case "3":
+                        mergedDictionaries.Clear();
+                        mergedDictionaries.Add(new RedTheme());
+                        break;
+                    default:
+                        mergedDictionaries.Clear();
+                        mergedDictionaries.Add(new LightTheme());
+                        break;
+                }
+            }
         }
     }
     
--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -8,6 +8,7 @@ public partial class MainPage : ContentPage
     
     public MainPage()
     {
+        
         InitializeComponent();
         OnClear(this, null);
 
@@ -16,7 +17,8 @@ public partial class MainPage : ContentPage
     string Expressions = string.Empty;
     void OnSelectNumber(object sender, EventArgs e)
     {
-    
+       
+        AppTheme currentTheme = Application.Current.RequestedTheme;
         Button button = (Button)sender;
         string pressed = button.Text;
         if (this.resultText.Text == "0")
@@ -88,7 +90,34 @@ public partial class MainPage : ContentPage
     public void OnBackToAbout(object sender, EventArgs args)
     {
         var name=Application.Current.StyleId;
-        Application.Current.MainPage.Navigation.PushModalAsync( new NavigationPage( new About()), true);
+        Application.Current.MainPage.Navigation.PushAsync( new NavigationPage( new About()), true);
     }
-
+    public void OnSetThemes(object sender,EventArgs args)
+    {
+        
+       MenuFlyoutItem menu= (MenuFlyoutItem)sender;
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            switch (menu.CommandParameter)
+            {
+                case "1":
+                    mergedDictionaries.Clear();
+                    mergedDictionaries.Add(new LightTheme());
+                    break;
+                case "2":
+                    mergedDictionaries.Clear();
+                    mergedDictionaries.Add(new PinkTheme());
+                    break;
+                case "3":
+                    mergedDictionaries.Clear();
+                    mergedDictionaries.Add(new RedTheme());
+                    break;
+                default:
+                    mergedDictionaries.Clear();
+                    mergedDictionaries.Add(new LightTheme());
+                    break;
+            }
+        }
+    }
 }
